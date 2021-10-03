f6894a321f335b97d5bd35563c974dead03b4757
shraazp <shravyap99@gmail.com>
2021-10-03 19:46:23 +0530

    added:validation for entries

--- addressbook.js
+++ addressbook.js
@@ -1,23 +1,41 @@
 const prompt = require("prompt-sync")();
-const firstName = prompt("Enter the first name: ");
-const lastName = prompt("Enter the last name: ");
-const address = prompt("Enter the Address: ");
-const city = prompt("Enter the city: ");
-const state = prompt("Enter the State: ");
-const zip = prompt("Enter the ZIP: ");
-const phone = prompt("Enter the Phone number: ");
-const email = prompt("Enter the email id: ");
-//array to store the contacts which are json objects
-let addressBook=[
-    {
-        "firstName":firstName,
-        "lastName":lastName,
-        "address":address,
-        "city":city,
-        "state":state,
-        "zip":zip,
-        "phone":phone,
-        "email":email,
-    }
-]
-console.log(addressBook)
+const validation = require("./validation.js");
+
+let contact;
+let addContact = () => {
+  let firstName = prompt("Enter the first name: ");
+  let lastName = prompt("Enter the last name: ");
+  let address = prompt("Enter the Address: ");
+  let city = prompt("Enter the city: ");
+  let state = prompt("Enter the State: ");
+  let zip = prompt("Enter the ZIP: ");
+  let phone = prompt("Enter the Phone number: ");
+  let email = prompt("Enter the email id: ");
+  try {
+    validation.firstNameValidation(firstName);
+    validation.lastNameValidation(lastName);
+    validation.addressValidation(address);
+    validation.cityValidation(city);
+    validation.stateValidation(state);
+    validation.zipValidation(zip);
+    validation.phoneValidation(phone);
+    validation.emailValidation(email);
+  } catch (error) {
+    console.error(error);
+    addContact();
+  }
+
+  contact = {
+      firstName: firstName,
+      lastName: lastName,
+      address: address,
+      city: city,
+      state: state,
+      zip: zip,
+      phone: phone,
+      email: email,
+    };
+    console.log("Contacts inserted successfully");
+    console.log(contact);
+};
+addContact();
